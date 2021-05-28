b4e4963085f998e4180bee5307a958090b626ee0
tschanik <84989816+tschanik@users.noreply.github.com>
2021-05-28 21:01:17 +0200

    Initial commit

---
+++ waschmaschine.ino
@@ -0,0 +1,235 @@
+#include <WiFi.h>
+#include <WebServer.h>
+
+#include <Wire.h>
+#include <Adafruit_ADS1X15.h>
+
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h> 
+
+#include <HTTPClient.h>
+
+#include "credential.h"
+
+Adafruit_ADS1115 ads1115; // Construct an ads1115
+
+//Netzspannung 230 V ±10 %
+
+// Laut Hersteller 30[A/V]
+// Kalibrierung erfolgte mit Haushaltsgeräten
+const float factor = 0.030616891; // [A/mv]
+int sample_rate = 2000; // [ms] zwei Sekunden lang messen
+int strompreis = 30; // [cent/kWh]
+
+String hostname = "Waschmaschine";
+
+WebServer server(80);
+
+// database
+HTTPClient http;
+  
+void WiFiConnect() {
+  WiFi.mode(WIFI_STA); // prevent sleeping
+  WiFi.setHostname(hostname.c_str()); //define hostname
+  WiFi.begin(ssid, password);
+  int count = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Connecting to WiFi..");
+    delay(1000);
+    // schould take longer than 10 seconds
+    count = count + 1;
+    if (count > 10){
+      // wait 2 minutes before try again
+      delay(2*60*1000);
+      ESP.restart();
+    }
+  }
+  // Print ESP32 Local IP Address
+  Serial.println("Connected");
+  Serial.println(WiFi.localIP());
+}
+
+void sendMessage() {
+  WiFiClientSecure client;
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  UniversalTelegramBot bot(BOTtoken, client);
+  String mes = "Hallo Mama,\n\n";
+  mes += "ich bin nun unter " + WiFi.localIP().toString() + " erreichbar.";
+  bot.sendMessage(CHAT_ID, mes, ""); // https://core.telegram.org/bots/api#formatting-options
+}
+
+float getcurrent() {
+  int bits;
+  float voltage;
+  float current;
+  float current_sum = 0;
+  int i = 0;
+  unsigned long time_check = millis();
+
+  while(millis() - time_check < sample_rate){
+    
+    bits = ads1115.readADC_Differential_0_1();
+    // threshold for measurement
+    if (abs(bits) <= 6) {
+      bits = 0;
+    }
+    // 16 bit -> 2^(16-1) = 32768 bits  4x gain: 1.024V -> 1.024V/32768bits
+    voltage = bits * (1.024*1000)/32768; // [mv]
+    current = voltage * factor; // [A]
+    current_sum += sq(current); // [A^2]
+    i = i + 1;
+ 
+  }  
+  
+  current = sqrt(current_sum/i); // [A]
+  return(current);
+  
+}
+
+
+String text= "";
+double current;
+float power;
+int p_max = 0;
+float E = 0;
+float kosten = 0;
+int stunde, minuten, sekunde;
+
+const String page PROGMEM = "<head>"
+            " <title>Waschmaschine</title>"
+            " <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js\"></script>"
+            " <meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">"
+            " <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+            " <style>"
+            " body {color: #eee;"
+            " background: #121212;"
+            " font: 100% system-ui;}"
+            " a {color: #809fff;}"
+            " h2 {display: inline-block; margin: .2em;}"
+            " </style>"
+            " </head>"
+            " <body>"
+            " <h1>Waschmaschine</h1>"
+            " <h2>Strom:</h2><h2 id=\"current\">-,---</h2><h2>A</h2><br>\r\n"
+            " <h2>Leistung:</h2><h2 id=\"power\">-,--</h2><h2>W</h2><br>\r\n" 
+            " <h2>P<sub>maximal</sub>:</h2><h2 id=\"p_max\">-</h2><h2>W</h2><br>\r\n"
+            " <h2>Energie:</h2><h2 id=\"energie\">-,--</h2><h2>Wh</h2><br>\r\n"
+            " <h2>Kosten:</h2><h2 id=\"kosten\">-,--</h2><h2>€</h2><br>\r\n"
+            " <h2>Läuft seit:</h2><h2 id=\"runtime\">--:--:--</h2><br>\r\n"
+            " <script>\r\n"
+            " $(document).ready(function(){\r\n"
+            " setInterval(getData,2000);\r\n"
+            " function getData(){\r\n"
+            " $.ajax({\r\n"
+            "  type:\"GET\",\r\n"
+            "  url:\"data\",\r\n"
+            "  success: function(data){\r\n"
+            "  var s = data.split(\'-\');\r\n"
+            "  $('#current').html(s[0].replace(\'.\',\',\'));\r\n"
+            "  $('#power').html(s[1].replace(\'.\',\',\'));\r\n"
+            "  $('#p_max').html(s[2]);\r\n"
+            "  $('#energie').html(s[3].replace(\'.\',\',\'));\r\n"
+            "  $('#kosten').html(s[4].replace(\'.\',\',\'));\r\n"
+            "  $('#runtime').html(s[5]);\r\n"
+            "}\r\n"
+            "}).done(function() {\r\n"
+            "  console.log('ok');\r\n"
+            "})\r\n"
+            "}\r\n"
+            "});"
+            "</script>\r\n"
+            "</body>";
+            
+void setup() {
+  Serial.begin(115200);
+
+  WiFiConnect(); 
+  Serial.print("RRSI: ");
+  Serial.println(WiFi.RSSI());
+
+  // ads.setGain(GAIN_FOUR); // 4x gain   +/- 1.024V  1 bit = 0.03125mV
+  ads1115.setGain(GAIN_FOUR);
+  ads1115.setDataRate(RATE_ADS1115_860SPS);
+  ads1115.begin(); // Initialize ads1015 at the default address 0x48
+  
+  server.on("/data", [](){
+    text = String(current,3);
+    text += "-";
+    text += (String)power;
+    text += "-";
+    text += (String)p_max;
+    text += "-";
+    text += (String)E;
+    text += "-";
+    text += (String)kosten;
+    text += "-";
+    if(stunde < 10){
+      text += "0";
+    }
+    text += String(stunde);
+    text += ":";
+    if(minuten < 10){
+      text += "0";
+    }
+    text += (String)minuten;
+    text += ":";
+    if(sekunde < 10){
+      text += "0";
+    }
+    text += (String)sekunde;
+    //Serial.println(text);
+    server.send(200, "text/plain", text);
+  });
+ 
+  server.on("/", []() {
+   server.send(200, "text/html", page);
+  });
+ 
+  server.begin();
+  Serial.println("HTTP server started");
+
+  http.setReuse(true);
+  
+  sendMessage();
+ 
+}
+
+void loop() {
+  
+  server.handleClient();
+  delay(2);//allow the cpu to switch to other tasks
+  if (WiFi.status() != WL_CONNECTED){
+    WiFiConnect(); 
+  }
+
+  current = getcurrent();
+
+  http.begin(webpage);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  http.POST("current=" + String(current, 3) + "&key=" + key);
+  http.end();
+  
+  power = 230 * current; // [W]
+  E = E + power * sample_rate/(1000*60*60); // [Wh]
+
+  kosten = (E * strompreis) / 100000 ; // [€]
+  
+  if(power > p_max) {
+    p_max = round(power);
+  }
+  
+  sekunde = (millis()/1000) % 60;
+  minuten  = (millis()/60000) % 60;
+  stunde  = (millis()/3600000) % 24;
+
+
+  /*
+  Serial.print("Strom: ");
+  Serial.print(current, 6);
+  Serial.print(" A | ");
+  Serial.print("Power: ");
+  Serial.print(power,6);
+  Serial.println(" W");
+  */
+  
+}
