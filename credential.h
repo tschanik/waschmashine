d9452fc500b58eb71d2543b796efbd61f15f4ca3
tschanik <84989816+tschanik@users.noreply.github.com>
2022-03-20 14:59:47 +0100

    Credentail Datei hinzugefügt

---
+++ credential.h
@@ -0,0 +1,14 @@
+#define ssid ""  // WiFi name
+#define password "" // WiFi password
+
+// Initialize Telegram BOT
+#define BOTtoken ""  // your Bot Token (Get from Botfather)
+
+// Use @myidbot to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+#define CHAT_ID ""
+
+#define webpage ""
+
+#define key ""
